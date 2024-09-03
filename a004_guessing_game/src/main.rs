45243a188791f76114b2f02d35fe7816d09da477
Sher-Chowdhury <sherc1980@yahoo.com>
2024-09-03 19:46:51 +0100

    wip

--- a004_guessing_game/src/main.rs
+++ a004_guessing_game/src/main.rs
@@ -1,5 +1,7 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+
 
 
 fn main() {
@@ -13,9 +15,20 @@ fn main() {
 
     let mut guess = String::new();
 
+
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {guess}");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+
+        println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
