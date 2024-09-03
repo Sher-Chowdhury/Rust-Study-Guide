2fb5d784b089ab73e5cba28d3cc65b386c36d2b5
Sher-Chowdhury <sherc1980@yahoo.com>
2024-09-03 20:34:27 +0100

    wip

--- a004_guessing_game/src/main.rs
+++ a004_guessing_game/src/main.rs
@@ -6,30 +6,36 @@ use rand::Rng;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
+    loop {
+        let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("Please input your guess.");
+        println!("The secret number is: {secret_number}");
 
-    let mut guess = String::new();
+        println!("Please input your guess.");
 
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new();
 
 
-    
-    // we effectively overridden the guess variable into a integer data type. 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
 
-    println!("You guessed: {guess}");
+        
+        // we effectively overridden the guess variable into a integer data type. 
+        let guess: u32 = guess.trim().parse().expect("Please type a number!\n\n\n");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
